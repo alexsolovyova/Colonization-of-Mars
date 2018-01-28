a0c121053a791eec4cbe31d69e6117761aaf0022
Solovyova Aleksandra <solovyovaa@yahoo.com>
2018-01-28 20:00:58 +0300

    Add preload-button

--- MainPage.js
+++ MainPage.js
@@ -36,3 +36,7 @@ function initQuoteCarousel() {
 $(function () {
     initQuoteCarousel();
 }); 
+
+var hellopreloader = document.getElementById("hellopreloader_preload");
+
+	    function fadeOutnojquery(el){el.style.opacity = 1;var interhellopreloader = setInterval(function(){el.style.opacity = el.style.opacity - 0.05;if (el.style.opacity <=0.05){ clearInterval(interhellopreloader);hellopreloader.style.display = "none";}},16);}window.onload = function(){setTimeout(function(){fadeOutnojquery(hellopreloader);},1000);};
