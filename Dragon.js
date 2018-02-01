92d0f8c44a29638e40e012a3cc59441ea3e92626
Solovyova Aleksandra <solovyovaa@yahoo.com>
2018-02-01 15:57:04 +0300

    Add files via upload

--- Dragon.js
+++ Dragon.js
@@ -1,52 +1,20 @@
 
-// makes the parallax elements
-function parallaxIt() {
-  // create variables
-  var $fwindow = $(window);
-  var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+$(document).ready(function(){
+  $(window).scroll(function(){
+    var scrollTop = $(window).scrollTop();
+    if (scrollTop > 49) {
+        $('body').addClass('header-fixed');
+    } else {
+        $('body').removeClass('header-fixed');
+    }
 
-  var $contents = [];
-  var $backgrounds = [];
-
-  // for each of content parallax element
-  $('[data-type="content"]').each(function(index, e) {
-    var $contentObj = $(this);
-
-    $contentObj.__speed = ($contentObj.data('speed') || 1);
-    $contentObj.__fgOffset = $contentObj.offset().top;
-    $contents.push($contentObj);
+    var topOffset = $('#demosection2').offset().top;
+    var headerHeight = $('#topnav').height();
+    var transitionPoint = topOffset - headerHeight;
+    if (scrollTop > transitionPoint) {
+        $('#topnav').addClass('alt-header');
+    } else {
+        $('#topnav').removeClass('alt-header');
+    }
   });
-
-  // for each of background parallax element
-  $('[data-type="background"]').each(function() {
-    var $backgroundObj = $(this);
-
-    $backgroundObj.__speed = ($backgroundObj.data('speed') || 1);
-    $backgroundObj.__fgOffset = $backgroundObj.offset().top;
-    $backgrounds.push($backgroundObj);
-  });
-
-  // update positions
-  $fwindow.on('scroll resize', function() {
-    scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-
-    $contents.forEach(function($contentObj) {
-      var yPos = $contentObj.__fgOffset - scrollTop / $contentObj.__speed;
-
-      $contentObj.css('top', yPos);
-    })
-
-    $backgrounds.forEach(function($backgroundObj) {
-      var yPos = -((scrollTop - $backgroundObj.__fgOffset) / $backgroundObj.__speed);
-
-      $backgroundObj.css({
-        backgroundPosition: '50% ' + yPos + 'px'
-      });
-    });
-  });
-
-  // triggers winodw scroll for refresh
-  $fwindow.trigger('scroll');
-};
-
-parallaxIt();
+});
