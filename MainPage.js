e3161d707a8da1bf9556ba8e1391e56d75fc02a5
Solovyova Aleksandra <solovyovaa@yahoo.com>
2018-02-02 23:02:12 +0300

    Add files via upload

--- MainPage.js
+++ MainPage.js
@@ -13,7 +13,38 @@ $(document).ready(function(){
   });
 });
 
+function initQuoteCarousel() {
 
+    var $quotesWrapper = $(".cust-quotes");
+    var $quotes = $quotesWrapper.find("blockquote");
+
+    if (!$quotes.length) {
+        return;
+    }
+
+    var selectNextQuote = function () {
+        // keep move first quote in dom to the end to make continous
+        var $quote = $quotesWrapper.find("blockquote:first").detach().appendTo($quotesWrapper);
+
+        setTimeout(selectNextQuote, $quote.data("timeout"));
+    };
+
+    setTimeout(selectNextQuote, $quotes.filter(":first").data("timeout"));
+
+}
+
+$(function () {
+    initQuoteCarousel();
+}); 
+
+var hellopreloader = document.getElementById("hellopreloader_preload");
+
+	    function fadeOutnojquery(el){el.style.opacity = 1;var interhellopreloader = setInterval(function(){el.style.opacity = el.style.opacity - 0.05;if (el.style.opacity <=0.05){ clearInterval(interhellopreloader);hellopreloader.style.display = "none";}},16);}window.onload = function(){setTimeout(function(){fadeOutnojquery(hellopreloader);},1000);};
+		
+		
+		
+		
+		
 var hellopreloader = document.getElementById("hellopreloader_preload");
 
 	    function fadeOutnojquery(el){el.style.opacity = 1;var interhellopreloader = setInterval(function(){el.style.opacity = el.style.opacity - 0.05;if (el.style.opacity <=0.05){ clearInterval(interhellopreloader);hellopreloader.style.display = "none";}},16);}window.onload = function(){setTimeout(function(){fadeOutnojquery(hellopreloader);},1000);};
