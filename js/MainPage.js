d5b9317038a768e9205460b67614d20573a8c1c1
Solovyova Aleksandra <solovyovaa@yahoo.com>
2018-02-05 16:46:46 +0300

    Add files via upload

--- js/MainPage.js
+++ js/MainPage.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:js/MainPage.js
 
   
 $(document).ready(function(){
@@ -49,55 +48,3 @@ var hellopreloader = document.getElementById("hellopreloader_preload");
 var hellopreloader = document.getElementById("hellopreloader_preload");
 
 	    function fadeOutnojquery(el){el.style.opacity = 1;var interhellopreloader = setInterval(function(){el.style.opacity = el.style.opacity - 0.05;if (el.style.opacity <=0.05){ clearInterval(interhellopreloader);hellopreloader.style.display = "none";}},16);}window.onload = function(){setTimeout(function(){fadeOutnojquery(hellopreloader);},1000);};
-=======
-
-  
-$(document).ready(function(){
-  $(window).scroll(function(){
-    var scrollTop = $(window).scrollTop();
-    if (scrollTop > 49) {
-        $('body').addClass('header-fixed');
-    } else {
-        $('body').removeClass('header-fixed');
-    }
-
-   
-  });
-});
-
-function initQuoteCarousel() {
-
-    var $quotesWrapper = $(".cust-quotes");
-    var $quotes = $quotesWrapper.find("blockquote");
-
-    if (!$quotes.length) {
-        return;
-    }
-
-    var selectNextQuote = function () {
-        // keep move first quote in dom to the end to make continous
-        var $quote = $quotesWrapper.find("blockquote:first").detach().appendTo($quotesWrapper);
-
-        setTimeout(selectNextQuote, $quote.data("timeout"));
-    };
-
-    setTimeout(selectNextQuote, $quotes.filter(":first").data("timeout"));
-
-}
-
-$(function () {
-    initQuoteCarousel();
-}); 
-
-var hellopreloader = document.getElementById("hellopreloader_preload");
-
-	    function fadeOutnojquery(el){el.style.opacity = 1;var interhellopreloader = setInterval(function(){el.style.opacity = el.style.opacity - 0.05;if (el.style.opacity <=0.05){ clearInterval(interhellopreloader);hellopreloader.style.display = "none";}},16);}window.onload = function(){setTimeout(function(){fadeOutnojquery(hellopreloader);},1000);};
-		
-		
-		
-		
-		
-var hellopreloader = document.getElementById("hellopreloader_preload");
-
-	    function fadeOutnojquery(el){el.style.opacity = 1;var interhellopreloader = setInterval(function(){el.style.opacity = el.style.opacity - 0.05;if (el.style.opacity <=0.05){ clearInterval(interhellopreloader);hellopreloader.style.display = "none";}},16);}window.onload = function(){setTimeout(function(){fadeOutnojquery(hellopreloader);},1000);};
->>>>>>> 7ed5b0e452cd92a12253cc93087d3949e63de547:MainPage.js
